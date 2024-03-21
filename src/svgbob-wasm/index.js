9e93d5c96775b94993a6ea45b4c9a9d35696dade
EmNudge <24513691+EmNudge@users.noreply.github.com>
2024-03-20 19:22:09 -0700

    move init outside of function call

--- src/svgbob-wasm/index.js
+++ src/svgbob-wasm/index.js
@@ -6,24 +6,29 @@ import {
 } from "./svgbob_wasm_bg.js";
 import wasmText from './base64File.js';
 
+const text = atob(wasmText);
+const bytes = new Uint8Array(text.length);
+for (let i = 0; i < text.length; i++) {
+  bytes[i] = text.charCodeAt(i);
+}
+
+const wasm = WebAssembly.instantiate(bytes, {
+  "./svgbob_wasm_bg.js": {
+    __wbindgen_object_drop_ref,
+    __wbindgen_throw,
+  },
+});
+
 let hasLoaded = false;
+wasm.then(({ instance }) => {
+  __wbg_set_wasm(instance.exports);
+  hasLoaded = true;
+});
 
 export const getRenderer = async () => {
   if (hasLoaded) return render;
 
-  const text = atob(wasmText);
-  const bytes = new Uint8Array(text.length);
-  for (let i = 0; i < text.length; i++) {
-    bytes[i] = text.charCodeAt(i);
-  }
-  
-  const wasm = await WebAssembly.instantiate(bytes, {
-    "./svgbob_wasm_bg.js": {
-      __wbindgen_object_drop_ref,
-      __wbindgen_throw,
-    },
-  });
-  __wbg_set_wasm(wasm.instance.exports);
+  await wasm;
   
   return render;
 };
