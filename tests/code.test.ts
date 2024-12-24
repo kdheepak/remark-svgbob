ca5ef40d89931ebda1affa909aad7031617256ef
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2024-12-24 16:14:16 -0500

    chore(test): Add tests for with and without mdx (#9)

--- tests/code.test.ts
+++ tests/code.test.ts
@@ -1,13 +1,11 @@
 import { test, expect } from "vitest";
-import svgBobCode from "../src/index";
+import remarkSVGBob from "../src/index";
 import { remark } from "remark";
 import remarkMDX from "remark-mdx";
 
-test("converts svgbob ascii to svg", async () => {
-  const content = "svg";
+const runTest = async (useMdx: boolean) => {
+  const content = "<svg";
   const md = `
-Use a code block with the language \`svgbob\`
-
 \`\`\`svgbob
        .---.
       /-o-/--
@@ -18,16 +16,34 @@ Use a code block with the language \`svgbob\`
        '
 \`\`\`
   `.trim();
-  const processor = remark().use(remarkMDX).use(svgBobCode);
+  const processor = useMdx
+    ? remark().use(remarkMDX).use(remarkSVGBob, { useMdx })
+    : remark().use(remarkSVGBob, { useMdx });
   const result = await processor.process(md);
   console.log(result.toString());
   expect(result.toString()).toContain(content);
+};
+
+test("converts svgbob ascii to svg without mdx", async () => {
+  await runTest(false);
 });
 
-test('works fine with multiple calls', async () => {
-  const processor = remark().use(remarkMDX).use(svgBobCode);
+test("converts svgbob ascii to svg with mdx", async () => {
+  await runTest(true);
+});
+
+test("works fine with multiple calls with mdx", async () => {
+  const processor = remark().use(remarkMDX).use(remarkSVGBob, { useMdx: true });
   await processor.process(`\`\`\`svgbob\nn\n\`\`\``);
   await processor.process(`\`\`\`svgbob\ni\n\`\`\``);
   await processor.process(`\`\`\`svgbob\nc\n\`\`\``);
   await processor.process(`\`\`\`svgbob\ne\n\`\`\``);
-})
+});
+
+test("works fine with multiple calls without mdx", async () => {
+  const processor = remark().use(remarkSVGBob, { useMdx: false });
+  await processor.process(`\`\`\`svgbob\nn\n\`\`\``);
+  await processor.process(`\`\`\`svgbob\ni\n\`\`\``);
+  await processor.process(`\`\`\`svgbob\nc\n\`\`\``);
+  await processor.process(`\`\`\`svgbob\ne\n\`\`\``);
+});
