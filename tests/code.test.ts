fd143651136a8e42188dbc4efef22b143a89a01f
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2024-12-24 08:53:48 -0500

    chore(test): Add tests for with and without mdx

--- tests/code.test.ts
+++ tests/code.test.ts
@@ -3,7 +3,7 @@ import svgBobCode from "../src/index";
 import { remark } from "remark";
 import remarkMDX from "remark-mdx";
 
-test("converts svgbob ascii to svg", async () => {
+const runTest = async (useMDX: boolean) => {
   const content = "svg";
   const md = `
 Use a code block with the language \`svgbob\`
@@ -18,14 +18,30 @@ Use a code block with the language \`svgbob\`
        '
 \`\`\`
   `.trim();
-  const processor = remark().use(remarkMDX).use(svgBobCode);
+  const processor = useMDX ? remark().use(remarkMDX).use(svgBobCode) : remark().use(svgBobCode);
   const result = await processor.process(md);
   console.log(result.toString());
   expect(result.toString()).toContain(content);
+};
+
+test("converts svgbob ascii to svg with mdx", async () => {
+  await runTest(true);
 });
 
-test('works fine with multiple calls', async () => {
+test("converts svgbob ascii to svg without mdx", async () => {
+  await runTest(false);
+});
+
+test('works fine with multiple calls with mdx', async () => {
   const processor = remark().use(remarkMDX).use(svgBobCode);
+  await processor.process(`\`\`\`svgbob\nn\n\`\`\``);
+  await processor.process(`\`\`\`svgbob\ni\n\`\`\``);
+  await processor.process(`\`\`\`svgbob\nc\n\`\`\``);
+  await processor.process(`\`\`\`svgbob\ne\n\`\`\``);
+})
+
+test('works fine with multiple calls without mdx', async () => {
+  const processor = remark().use(svgBobCode);
   await processor.process(`\`\`\`svgbob\nn\n\`\`\``);
   await processor.process(`\`\`\`svgbob\ni\n\`\`\``);
   await processor.process(`\`\`\`svgbob\nc\n\`\`\``);
