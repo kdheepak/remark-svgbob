84f7429c7effa5c4059e58576a85467b1e97ff56
Matt Spencer <matthew@thespencers.me.uk>
2024-12-24 13:48:19 +0000

    Inline SVG in html document (#7)
    Generate inline SVG without the need for inserting a 'raw' node.
    It removes the inline base64 encoded SVG in a <img> tag as this did now allow style variations.
    The css styling has been removed from the embedded SVG to enable site-wide control of the svgbob styling.
    This allows for dealing with cases such as dark mode etc
    
    Signed-off-by: Matt Spencer matthew@thespencers.me.uk

--- src/remarkPlugin.ts
+++ src/remarkPlugin.ts
@@ -1,35 +1,87 @@
 import { visit } from "unist-util-visit";
 import type { Node } from "unist";
 import { getRenderer } from "./svgbob-wasm";
-import { Buffer } from "node:buffer";
+import { fromHtml } from 'hast-util-from-html'
+import { MdxJsxAttribute, MdxJsxFlowElement } from 'mdast-util-mdx-jsx'
+import { Root, Element as HASTElement, Text as HASTText, Properties, RootContent} from "hast";
 
 interface CodeNode extends Node {
   lang?: string;
   value: string;
 }
 
+// Convert HAST attributes into MdxJsx attributes
+function processHastAttributes(properties: Properties) : MdxJsxAttribute[] {
+  function get_value_as_string(key) : string {
+    let value = properties[key];
+    if(value instanceof Array) {
+      return value.map((v) => v.toString()).join(" ");
+    }
+    else if(typeof value === "number") {
+      return properties[key].toString();
+    }
+    else if (typeof value === "string") {
+      return properties[key] as string;
+    }
+  };
+
+  return Object.keys(properties).map((key) => {
+    return {
+      type: "mdxJsxAttribute",
+      name: key,
+      value: get_value_as_string(key),
+    }
+  });
+}
+
+function isElement(node: RootContent) : node is HASTElement {
+  return node?.type == "element";
+}
+
+function isText(node: RootContent) : node is HASTText {
+  return node?.type == "text";
+}
+
+function processHastElement(node: RootContent) : MdxJsxFlowElement | undefined {
+  let ret = undefined;
+  if(isElement(node)) {
+    // Deal with text nodes seperately so that we don't recursively place whitespace only 
+    // nodes in the output tree which will litter the html with empty <text> nodes.
+    if(node.tagName === "text" && node?.children.length == 1 && isText(node.children[0])) { 
+      ret = {
+        type: "mdxJsxFlowElement",
+        name: node.tagName,
+        attributes: processHastAttributes(node.properties),
+        children: [{type: "text", value: node.children[0].value}],
+      }
+    } else {
+      ret = {
+        type: "mdxJsxFlowElement",
+        name: node.tagName,
+        attributes: processHastAttributes(node.properties),
+        children: node?.children.map((child) => processHastElement(child)).filter((n) => n!=undefined) || [],
+      }
+    }
+  }
+
+  return ret;
+}
+
+
 export const remarkPlugin = () => {
   return async function transformer(tree: Node): Promise<Node> {
     const render = await getRenderer();
-    // Create an array to hold all of the images from the markdown file
 
     visit(tree, "code", (node: CodeNode, index: any, parent: any) => {
       if (node.lang === "svgbob") {
         const svg = render(node.value);
-        const buffer = Buffer.from(svg, "utf8");
-        const base64SVG = buffer.toString("base64");
-        const imgSrc = `data:image/svg+xml;base64,${base64SVG}`;
-
-        const image = {
-          type: "mdxJsxFlowElement",
-          name: "img",
-          attributes: [
-            { type: "mdxJsxAttribute", name: "src", value: imgSrc },
-            { type: "mdxJsxAttribute", name: "alt", value: "svgbob" },
-          ],
-          children: [],
-        };
-
+        // Convert into hast format
+        let hast : Root = fromHtml(svg, {fragment: true});
+        // The first (and only) child of the root node will an svg element
+        let element = hast.children[0];
+        // Recursively process the elements into mdxJsxFlowElements
+        let image : MdxJsxFlowElement = processHastElement(element);
+        // Replace the code node with the image node
         parent.children.splice(index, 1, image);
       }
     });
@@ -37,3 +89,4 @@ export const remarkPlugin = () => {
     return tree;
   };
 };
+
